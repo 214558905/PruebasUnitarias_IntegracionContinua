c3cb6582e433cb0fd898e34135cbed261ef55145
ales <alexnaula57@gmail.com>
2021-05-05 13:07:21 -0500

    CREATE CLASS NUMBER AND CREATE METHOD GETADDITION

---
+++ src/test/java/NumberTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NumberTest {
+
+private Number number;
+	
+	@Before
+	public void Before() {
+		
+		this.number = new Number(9, 9);
+		
+	}
+	@Test
+	public void getAddition() {
+
+		assertEquals(18, this.number.getAddition());
+	}
+
+}
